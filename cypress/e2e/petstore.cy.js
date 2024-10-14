0c479f85f3a8089d1b0f381be0e8476676613a5d
Faylix30 <101813100+Faylix30@users.noreply.github.com>
2024-10-14 15:20:04 +0700

    second commit

--- cypress/e2e/petstore.cy.js
+++ cypress/e2e/petstore.cy.js
@@ -1,4 +1,4 @@
-describe('login app commands', () => {
+describe('login', () => {
 
     beforeEach(() => {
         cy.visit('https://petstore.octoperf.com/actions/Account.action?signonForm=')
@@ -10,15 +10,15 @@ describe('login app commands', () => {
         cy.get('#MainImageContent > img').should('be.visible')
     })
 
-    it('Tc-buy fish', () => {
-        cy.get('#SidebarContent > [href="/actions/Catalog.action?viewCategory=&categoryId=FISH"] > img').click()
-        cy.url().should('include', 'https://petstore.octoperf.com/actions/Catalog.action?viewCategory=&categoryId=FISH')
+    it.only('Tc-buy bird', () => {
+        cy.get('#SidebarContent > [href="/actions/Catalog.action?viewCategory=&categoryId=BIRDS"] > img').click()
+        cy.url().should('include', 'https://petstore.octoperf.com/actions/Catalog.action?viewCategory=&categoryId=BIRDS')
         cy.get('h2').should('be.visible')
         cy.get('tbody > :nth-child(2) > :nth-child(1) > a').click()
-        cy.get(':nth-child(3) > :nth-child(1) > a').click()
+        cy.get('tbody > :nth-child(2) > :nth-child(1) > a').click()
         cy.get('.Button').click()
         cy.get(':nth-child(5) > input').clear()
-        cy.get(':nth-child(5) > input').type(5)
+        cy.get(':nth-child(5) > input').type(3)
         cy.get('[colspan="7"] > input').click()
         cy.get('[href="/actions/Order.action?newOrderForm="]').click()
         cy.get(':nth-child(1) > th').should('be.visible')
@@ -32,7 +32,7 @@ describe('login app commands', () => {
         cy.get('#BackLink > a').click()
     })
 
-    it.only('Tc-check add cart', () => {
+    it('Tc-check add cart', () => {
         cy.get('#SidebarContent > [href="/actions/Catalog.action?viewCategory=&categoryId=FISH"] > img').click()
         cy.url().should('include', 'https://petstore.octoperf.com/actions/Catalog.action?viewCategory=&categoryId=FISH')
         cy.get('h2').should('be.visible')
@@ -57,22 +57,38 @@ describe('login app commands', () => {
         cy.contains('EST-15').should('be.visible')
     })
 
-    it('Tc-search', () => {
-        cy.get('[size="14"]').type('Birds')
+    it('Tc-search Dogs', () => {
+        cy.get('[size="14"]').type('Dogs')
         cy.get('[name="searchProducts"]').click()
     })
 
+    it('Tc-search on text', () => {
+        cy.get('[name="searchProducts"]').click()
+        cy.contains('Please enter a keyword to search for, then press the search button.').should('be.visible')
+        cy.get(':nth-child(1) > li').should('be.visible')
+        cy.get(':nth-child(2) > li').should('be.visible')
+        cy.get('#LogoContent > a > img').click()
+    })
+
     it('Tc-sign out', () => {
-        cy.get('[href="/actions/Account.action?signoff="]').click()
+        cy.get('#MenuContent').within(() => {
+            cy.contains('Sign Out').click();
+        });
+        // cy.get('[href="/actions/Account.action?signoff="]').click()
         cy.get('[href="/actions/Account.action?signonForm="]').should('be.visible')
 
     })
 
-    it('Tc-06-error login', () => {
-        cy.get('[name="username"]').type('j2ee')
+})
+
+describe('error login', () => {
+    beforeEach(()=>{
+        cy.visit('https://petstore.octoperf.com/actions/Account.action?signonForm=')
+    })
+    it('Tc-error login', () => {
+        cy.get('[name="username"]').type('hello')   
         cy.get('[name="password"]').type('j2ee')
         cy.get('[name="signon"]').click()
         cy.contains('Please enter your username and password.').should('be.visible')
     })
-
 })
